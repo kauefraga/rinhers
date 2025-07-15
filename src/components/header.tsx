674c39c15512519ed88dd11defc4409617132fb6
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-07-14 23:59:09 -0300

    feat: create Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import { Button } from './button';
+import { ThemeToggle } from './theme-toggle';
+
+export function Header() {
+  return (
+    <header className="flex justify-between">
+      <h1 className="text-2xl font-bold">Rinhers™</h1>
+
+      <div className="flex gap-8">
+        <Button disabled title="Em breve...">
+          Atualizar
+        </Button>
+
+        <ThemeToggle />
+      </div>
+    </header>
+  );
+}
