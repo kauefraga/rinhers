e7c67fc1291e36cfe3e01f44aa4b2e0626f78695
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-07-16 16:41:03 -0300

    feat: create <Footer> component

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Alert } from './components/alert';
+import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { Link } from './components/link';
 import { SearchInput } from './components/search-input';
@@ -20,75 +21,77 @@ export function App() {
   );
 
   return (
-    <div className="w-full max-w-5xl mx-auto flex flex-col mt-3">
-      <Header />
+    <>
+      <div className="w-full max-w-5xl mx-auto flex flex-col mt-3">
+        <Header />
 
-      <main>
-        <div className="flex items-center max-w-3xl mx-auto">
-          <img src="/banner.jpg" alt="Banner da Rinha de Back end escrito 'Rinha de Back end'" width="400" height="250" />
+        <main>
+          <div className="flex items-center max-w-3xl mx-auto">
+            <img src="/banner.jpg" alt="Banner da Rinha de Back end escrito 'Rinha de Back end'" width="400" height="250" />
 
-          <p>
-            A rinha de Back end é uma competição super divertida.
-            Veja mais sobre a Rinha 2025 no
-            {' '}
-            <Link href="https://github.com/zanfranceschi/rinha-de-backend-2025">
-              repositório oficial
-            </Link>
-            {' '}
-            e no
-            {' '}
-            <Link href="https://x.com/search?q=rinha%20de%20backend">
-              Twitter
-            </Link>
-            .
-          </p>
-        </div>
-
-        <div className="flex items-center gap-5">
-          <SearchInput
-            placeholder="Digite o nome da submissão..."
-            onChange={(e) => { setSearch(e.target.value); }}
-            value={search}
-          />
-
-          <div className="text-right">
             <p>
-              {summary.successful_downloads}
+              A rinha de Back end é uma competição super divertida.
+              Veja mais sobre a Rinha 2025 no
               {' '}
-              Rinhers no momento*
-            </p>
-            <p>
-              Dados de
+              <Link href="https://github.com/zanfranceschi/rinha-de-backend-2025">
+                repositório oficial
+              </Link>
               {' '}
-              {generatedAt}
+              e no
+              {' '}
+              <Link href="https://x.com/search?q=rinha%20de%20backend">
+                Twitter
+              </Link>
+              .
             </p>
           </div>
-        </div>
 
-        <ul className="my-5">
-          {filteredRinhers.map(r => <li>{r.name}</li>)}
-        </ul>
+          <div className="flex items-center gap-5">
+            <SearchInput
+              placeholder="Digite o nome da submissão..."
+              onChange={(e) => { setSearch(e.target.value); }}
+              value={search}
+            />
 
-        <Alert>
-          Essas informações foram todas tiradas do arquivo
-          {' '}
-          <code>`info.json`</code>
-          , adotado na edição 2025 da Rinha de Backend.
-        </Alert>
+            <div className="text-right">
+              <p>
+                {summary.successful_downloads}
+                {' '}
+                Rinhers no momento*
+              </p>
+              <p>
+                Dados de
+                {' '}
+                {generatedAt}
+              </p>
+            </div>
+          </div>
 
-        <Alert type="warning">
-          *
-          {' '}
-          {summary.failed_downloads}
-          {' '}
-          Rinhers sem o arquivo
-          {' '}
-          <code>`info.json`</code>
-          , que não estão inclusos na contagem 🙁
-        </Alert>
-      </main>
+          <ul className="my-5">
+            {filteredRinhers.map(r => <li>{r.name}</li>)}
+          </ul>
 
-      <footer></footer>
-    </div>
+          <Alert>
+            Essas informações foram todas tiradas do arquivo
+            {' '}
+            <code>`info.json`</code>
+            , adotado na edição 2025 da Rinha de Backend.
+          </Alert>
+
+          <Alert type="warning">
+            *
+            {' '}
+            {summary.failed_downloads}
+            {' '}
+            Rinhers sem o arquivo
+            {' '}
+            <code>`info.json`</code>
+            , que não estão inclusos na contagem 🙁
+          </Alert>
+        </main>
+      </div>
+
+      <Footer />
+    </>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,19 @@
+import { Link } from './link';
+
+export function Footer() {
+  return (
+    <footer className="mt-5 border-t-2 border-slate-600">
+      <div className="w-full max-w-5xl mx-auto flex justify-between py-4">
+        <p>
+          Feito por
+          {' '}
+          <Link href="https://kauefraga.dev" type="secondary">Kauê Fraga</Link>
+        </p>
+
+        <p>
+          <Link href="https://github.com/kauefraga/rinhers" type="secondary">Repositório Rinhers™</Link>
+        </p>
+      </div>
+    </footer>
+  );
+}
